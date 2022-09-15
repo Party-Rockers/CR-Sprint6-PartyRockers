44b85750f34e61df29cf6e6cb3b3a6801a48ed5a
kyle-slalom <113383537+kyle-slalom@users.noreply.github.com>
2022-09-15 15:35:40 -0400

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,8 +4,6 @@
 sudo apt-get update
 
 # Install depends if they are not installed
-sudo apt install awscli -y
-sudo apt install jq -y
 sudo apt install openjdk-17-jre-headless -y
 
 # Stop any server that might be running on port 8080.
@@ -14,7 +12,7 @@ sudo lsof -t -i tcp:8080 | xargs kill -9
 # Clean working folder
 rm -rf /home/ubuntu/java
 
-# Remap 8080 to 80
+# Remap 80 to 8080
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 # Save iptables
