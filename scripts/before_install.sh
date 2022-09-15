0ba22abba7073dbf3c39097aadb46dd364e6ad97
kyle-slalom <113383537+kyle-slalom@users.noreply.github.com>
2022-09-15 13:10:19 -0400

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,7 +7,6 @@ sudo apt-get update
 sudo apt install awscli -y
 sudo apt install jq -y
 sudo apt install openjdk-17-jre-headless -y
-sudo apt install iptables-persistent -y
 
 # Stop any server that might be running on port 8080.
 sudo lsof -t -i tcp:8080 | xargs kill -9
@@ -19,5 +18,5 @@ rm -rf /home/ubuntu/java
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 # Save iptables
-sudo iptables-save > /etc/iptables/rules.v4
-sudo ip6tables-save > /etc/iptables/rules.v6
+sudo /sbin/iptables-save > /etc/iptables/rules.v4
+sudo /sbin/ip6tables-save > /etc/iptables/rules.v6
