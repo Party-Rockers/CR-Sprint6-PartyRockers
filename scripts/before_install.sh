7eaf526bc70070fbba24a56efd0a3a0019142ddd
Kyle Lierer <kyle.lierer@slalom.com>
2022-09-19 12:32:09 -0400

    Fixed before script.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -14,11 +14,3 @@ rm -rf /home/ubuntu/java
 
 # Remap 80 to 8080
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
-
-# Save iptables
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-sudo apt-get -y install iptables-persistent
-
-sudo iptables-save > /etc/iptables/rules.v4
-sudo ip6tables-save > /etc/iptables/rules.v6
