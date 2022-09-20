04d32c16cc3caef94a1dd48bbd4817238e4c6fa5
Sarah Moreland <sarah.moreland@slalom.com>
2022-09-20 15:53:56 -0400

    Adding Maya's email to the sns subscription

--- codedeploy.tf
+++ codedeploy.tf
@@ -2,6 +2,28 @@ resource "aws_codedeploy_app" "cd" {
   name = "${var.default_tags.Name}-cd-app"
 }
 
+resource "aws_sns_topic" "sns" {
+  name = "${var.default_tags.Name}-sns"
+}
+
+resource "aws_sns_topic_subscription" "email-target1" {
+  topic_arn = aws_sns_topic.sns.arn
+  protocol  = "email"
+  endpoint  = "sarah.moreland@slalom.com"
+}
+
+resource "aws_sns_topic_subscription" "email-target2" {
+  topic_arn = aws_sns_topic.sns.arn
+  protocol  = "email"
+  endpoint  = "kyle.lierer@slalom.com"
+}
+
+resource "aws_sns_topic_subscription" "email-target3" {
+  topic_arn = aws_sns_topic.sns.arn
+  protocol  = "email"
+  endpoint  = "maya.rao@slalom.com"
+}
+
 resource "aws_codedeploy_deployment_group" "cd-group" {
   app_name              = aws_codedeploy_app.cd.name
   deployment_group_name = "${var.default_tags.Name}-cd-deployment-group"
