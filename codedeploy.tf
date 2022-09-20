8300f4ce9a6386b33da7333ab2c8ed03162f32ff
Kyle Lierer <kyle.lierer@slalom.com>
2022-09-20 15:49:36 -0400

    Added email to sns topic.

--- codedeploy.tf
+++ codedeploy.tf
@@ -6,10 +6,16 @@ resource "aws_sns_topic" "sns" {
   name = "${var.default_tags.Name}-sns"
 }
 
-resource "aws_sns_topic_subscription" "email-target" {
+resource "aws_sns_topic_subscription" "email-target1" {
   topic_arn = aws_sns_topic.sns.arn
   protocol  = "email"
   endpoint  = "sarah.moreland@slalom.com"
+}
+
+resource "aws_sns_topic_subscription" "email-target2" {
+  topic_arn = aws_sns_topic.sns.arn
+  protocol  = "email"
+  endpoint  = "kyle.lierer@slalom.com"
 }
 
 resource "aws_codedeploy_deployment_group" "cd-group" {
