d9861f60e749c71bacfa5edac9408b9b3c068ea6
Kyle Lierer <kyle.lierer@slalom.com>
2022-09-22 15:15:18 -0400

    Fixed naming error.

--- src/main/java/com/slalom/api/TestController.java
+++ src/main/java/com/slalom/api/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class TestController {
 
     private static final String template = "Test, %s!";
     private final AtomicLong counter = new AtomicLong();
