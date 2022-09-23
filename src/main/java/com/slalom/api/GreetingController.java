da06d221873824f824f0da5b6218168d9d5a238d
Sarah Moreland <sarah.moreland@slalom.com>
2022-09-23 10:46:42 -0400

    adding a comment for hello world API

--- src/main/java/com/slalom/api/GreetingController.java
+++ src/main/java/com/slalom/api/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
+    // API for hello world
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
